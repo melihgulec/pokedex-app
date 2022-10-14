bada3df5330d5db648a8674137046f0b62268952
unknown <melih.gulec14@gmail.com>
2022-10-14 18:43:59 +0300

    Add router and home page

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import {Text} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import Home from './pages/Home';
+
+const Stack = createNativeStackNavigator();
 
 const Router = () => {
-  return <Text></Text>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default Router;
