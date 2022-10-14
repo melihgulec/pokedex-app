4d47e9f36a31aa4a768d8e9f83701aa26fa0eb88
unknown <melih.gulec14@gmail.com>
2022-10-14 22:23:20 +0300

    Added ColorFromType function

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -5,14 +5,21 @@ import TypeChip from '../TypeChip';
 import WhiteSpace from '../WhiteSpace/WhiteSpace';
 
 import styles from './PokemonCard.style';
+import getColorFromType from '../../utils/ColorFromType';
 
 const PokemonCard = ({pokemon}) => {
   const renderTypeChips = ({item}) => {
     return <TypeChip type={capitalize(item)} />;
   };
 
+  console.log(pokemon)
+
   return (
-    <View style={styles.container}>
+    <View
+      style={[
+        styles.container,
+        {backgroundColor: getColorFromType(pokemon.types[0])},
+      ]}>
       <View style={styles.infoContainer}>
         <Text style={styles.hashText}>
           #{pokemon.id.toString().padStart(3, '0')}
