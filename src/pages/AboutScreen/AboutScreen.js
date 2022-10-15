0559a62c76782d870e131a11d213ecb32ffa90f6
unknown <melih.gulec14@gmail.com>
2022-10-15 17:34:49 +0300

    Edited AboutScreen after InfoCard component changes

--- src/pages/AboutScreen/AboutScreen.js
+++ src/pages/AboutScreen/AboutScreen.js
@@ -6,37 +6,69 @@ import Title from '../../components/Title/Title';
 import WhiteSpace from '../../components/WhiteSpace/WhiteSpace';
 import capitalize from '../../utils/Capitalize';
 
+import styles from './AboutScreen.style';
+
 const AboutScreen = ({pokemon}) => {
   console.log(pokemon);
   return (
     <ScrollView>
-      <InfoCard title={'Species'} contentText={pokemon.species} />
+      <InfoCard
+        title={'Species'}
+        component={<Text style={styles.contentText}>{pokemon.species}</Text>}
+      />
       <WhiteSpace vertical />
-      <InfoCard title={'Height'} contentText={pokemon.height} />
+      <InfoCard
+        title={'Height'}
+        component={<Text style={styles.contentText}>{pokemon.height}</Text>}
+      />
       <WhiteSpace vertical />
-      <InfoCard title={'Weight'} contentText={pokemon.weight} />
+      <InfoCard
+        title={'Weight'}
+        component={<Text style={styles.contentText}>{pokemon.weight}</Text>}
+      />
       <WhiteSpace vertical />
-      <InfoCard title={'Abilities'} contentText={pokemon.species} />
+      <InfoCard
+        title={'Growth Rate'}
+        component={
+          <Text style={styles.contentText}>{pokemon.training.growthRate}</Text>
+        }
+      />
       <WhiteSpace vertical />
       <WhiteSpace vertical />
       <Title text={'Breeding'} />
       <WhiteSpace vertical />
       <InfoCard
         title={'Gender'}
-        contentText={`M: ${pokemon.breedings.gender.male} | F: ${pokemon.breedings.gender.female}`}
+        component={
+          <Text
+            style={
+              styles.contentText
+            }>{`M: ${pokemon.breedings.gender.male} | F: ${pokemon.breedings.gender.female}`}</Text>
+        }
       />
       <WhiteSpace vertical />
       <InfoCard
         title={'Egg Groups'}
-        contentText={pokemon.breedings.eggGroups.map(
-          (eggGroup, index) =>
-            `${capitalize(eggGroup)}${
-              index === pokemon.breedings.eggGroups.length - 1 ? '' : ', '
-            }`,
-        )}
+        component={
+          <Text style={styles.contentText}>
+            {pokemon.breedings.eggGroups.map(
+              (eggGroup, index) =>
+                `${capitalize(eggGroup)}${
+                  index === pokemon.breedings.eggGroups.length - 1 ? '' : ', '
+                }`,
+            )}
+          </Text>
+        }
       />
       <WhiteSpace vertical />
-      <InfoCard title={'Egg Cycle'} contentText={pokemon.breedings.eggCycles.value} />
+      <InfoCard
+        title={'Egg Cycle'}
+        component={
+          <Text style={styles.contentText}>
+            {pokemon.breedings.eggCycles.value}
+          </Text>
+        }
+      />
       <WhiteSpace vertical />
     </ScrollView>
   );
--- src/pages/AboutScreen/AboutScreen.style.js
+++ src/pages/AboutScreen/AboutScreen.style.js
@@ -1,5 +1,8 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-
+  contentText: {
+    flex: 2,
+    color: 'black',
+  },
 });
